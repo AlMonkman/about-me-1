1ee5b0b5a39d6c578fccb618f14ab527159e665b
JOHN GOULDMAN <johngouldman@pm.me>
2022-02-20 20:31:24 -0600

    added some js changes

--- js/app.js
+++ js/app.js
@@ -84,6 +84,7 @@ if (askJavascript.toLowerCase() === 'yes' || askJavascript.toLowerCase() ==='y')
 */
 
 let attemptsLeft = 4;
+
 while (attemptsLeft > 0) {
   let guessNumber = Number(prompt('Guess a number between 1-10'));
   if (guessNumber > 5) {
@@ -111,3 +112,27 @@ while (attemptsLeft > 0) {
 
 // Keep track of total number of correct answers.
 // At the end, tell them how many they got correct out of the 7 questions asked
+
+
+let favMovies = ['Scent of a Woman', 'Gattaca', 'The Devil\'s Advocate', 'Cinderella Man', 'Braveheart', 'V for Vendetta', 'Goodfellas', 'Casino', 'Training Day', 'Batman Begins'];
+// let answeredCorrectly = false;
+let attemptsLeft1 = 6;
+let correctAttempts = 0;
+
+while (attemptsLeft1) {
+  for (let j = 0; j < favMovies.length; j++) {
+    let userResponse = prompt('Guess one of my favorite movies.');
+    if (userResponse === favMovies[0] || userResponse === favMovies[1] || userResponse === favMovies[2] ||userResponse === favMovies[3] || userResponse === favMovies[4] || userResponse === favMovies[5] || userResponse === favMovies[6] || userResponse === favMovies[7] || userResponse === favMovies[8] || userResponse === favMovies[9] ) {
+      alert('You guessed one right! Try to guess another one.');
+      correctAttempts++;
+    } else if (userResponse !== favMovies[j]) {
+      alert(`That's not one! You have ${attemptsLeft1 - 1} attempts left!`)
+    }
+    attemptsLeft1--;
+    if (attemptsLeft1 === 0) {
+      alert('You ran out of tries! The answers are on my "About Me" page.');
+      break;
+    }
+  }
+}
+alert('You guessed ' + correctAttempts + ' out of 7 right!');
