96f9f4689f7e8658c687d616c1b67dac13936585
Alan Monkman <almonkman27@gmail.com>
2022-02-22 21:03:46 -0800

    Added function to fifth question

--- js/app.js
+++ js/app.js
@@ -9,85 +9,87 @@ firstQuestion();
 secondQuestion();
 thirdQuestion();
 fourthQuestion();
+fifthQuestion();
 
-function firstQuestion(){
-// First Question
-let askTexas = prompt('Am I from Texas?');
+function firstQuestion() {
+  // First Question
+  let askTexas = prompt('Am I from Texas?');
 
-if (askTexas.toLowerCase() === 'yes' || askTexas.toLowerCase() ==='y') {
-  //console.log('You are correct!');
-  alert('You are correct!');
-} else if (askTexas.toLowerCase() === 'no' || askTexas.toLowerCase() === 'n') {
-  //console.log('Wrong!');
-  alert('Wrong!');
-} else {
-  //console.log('"Yes" or "No" answers only, please!');
-  alert('"Yes" or "No" answers only, please!');
-}
-}
-
-function secondQuestion(){
-// Second Question
-let askAvocado = prompt('Do I like avocados?');
-
-if (askAvocado.toLowerCase() === 'yes' || askAvocado.toLowerCase() ==='y') {
-  //console.log('You guessed it! They\'re my favorite.');
-  alert('You guessed it! They\'re my favorite.');
-} else if (askAvocado.toLowerCase() === 'no' || askAvocado.toLowerCase() === 'n') {
-  //console.log('Sorry, but avocados are the best thing on Earth.');
-  alert('Sorry, but avocados are the best thing on Earth.');
-} else {
-  //console.log('"Yes" or "No" answers only, please!');
-  alert('"Yes" or "No" answers only, please!');
-}
+  if (askTexas.toLowerCase() === 'yes' || askTexas.toLowerCase() === 'y') {
+    //console.log('You are correct!');
+    alert('You are correct!');
+  } else if (askTexas.toLowerCase() === 'no' || askTexas.toLowerCase() === 'n') {
+    //console.log('Wrong!');
+    alert('Wrong!');
+  } else {
+    //console.log('"Yes" or "No" answers only, please!');
+    alert('"Yes" or "No" answers only, please!');
+  }
 }
 
-function thirdQuestion(){
-// Third Question
-let askMac = prompt('Are Macintosh computers too expensive?');
+function secondQuestion() {
+  // Second Question
+  let askAvocado = prompt('Do I like avocados?');
 
-if (askMac.toLowerCase() === 'yes' || askMac.toLowerCase() ==='y') {
-  //console.log('Yes, they are. They should be cheaper for coders.');
-  alert('Yes, they are. They should be cheaper for coders.');
-} else if (askMac.toLowerCase() === 'no' || askMac.toLowerCase() === 'n') {
-  //console.log('Not everyone is rich like you.');
-  alert('Not everyone is rich like you.');
-} else {
-  //console.log('"Yes" or "No" answers only, please!');
-  alert('"Yes" or "No" answers only, please!');
-}
-}
-
-function fourthQuestion(){
-// Fourth Question
-let askPoker = prompt('Is Texas Hold \'em a fun game?');
-
-if (askPoker.toLowerCase() === 'yes' || askPoker.toLowerCase() ==='y') {
-  //console.log('It\'s the best game!');
-  alert('It\'s the best game!');
-} else if (askPoker.toLowerCase() === 'no' || askPoker.toLowerCase() === 'n') {
-  //console.log('You really haven\'t played, have you?');
-  alert('You really haven\'t played, have you?');
-} else {
-  //console.log('"Yes" or "No" answers only, please!');
-  alert('"Yes" or "No" answers only, please!');
-}
+  if (askAvocado.toLowerCase() === 'yes' || askAvocado.toLowerCase() === 'y') {
+    //console.log('You guessed it! They\'re my favorite.');
+    alert('You guessed it! They\'re my favorite.');
+  } else if (askAvocado.toLowerCase() === 'no' || askAvocado.toLowerCase() === 'n') {
+    //console.log('Sorry, but avocados are the best thing on Earth.');
+    alert('Sorry, but avocados are the best thing on Earth.');
+  } else {
+    //console.log('"Yes" or "No" answers only, please!');
+    alert('"Yes" or "No" answers only, please!');
+  }
 }
 
-// Fifth Question
-let askJavascript = prompt('Do you really think I like learning Javascript?');
+function thirdQuestion() {
+  // Third Question
+  let askMac = prompt('Are Macintosh computers too expensive?');
 
-if (askJavascript.toLowerCase() === 'yes' || askJavascript.toLowerCase() ==='y') {
-  //console.log('Of course I do, ' + userName + '.' + ' Who doesn\'t!?');
-  alert('Of course I do, ' + userName + '.' + ' Who doesn\'t!?');
-} else if (askJavascript.toLowerCase() === 'no' || askJavascript.toLowerCase() === 'n') {
-  //console.log('You must not know anything about me, ' + username );
-  alert('You must not know anything about me, ' + userName + '!');
-} else {
-  //console.log('"Yes" or "No" answers only, please!');
-  alert('"Yes" or "No" answers only!' + ' Thank you, ' + userName + '.');
+  if (askMac.toLowerCase() === 'yes' || askMac.toLowerCase() === 'y') {
+    //console.log('Yes, they are. They should be cheaper for coders.');
+    alert('Yes, they are. They should be cheaper for coders.');
+  } else if (askMac.toLowerCase() === 'no' || askMac.toLowerCase() === 'n') {
+    //console.log('Not everyone is rich like you.');
+    alert('Not everyone is rich like you.');
+  } else {
+    //console.log('"Yes" or "No" answers only, please!');
+    alert('"Yes" or "No" answers only, please!');
+  }
 }
 
+function fourthQuestion() {
+  // Fourth Question
+  let askPoker = prompt('Is Texas Hold \'em a fun game?');
+
+  if (askPoker.toLowerCase() === 'yes' || askPoker.toLowerCase() === 'y') {
+    //console.log('It\'s the best game!');
+    alert('It\'s the best game!');
+  } else if (askPoker.toLowerCase() === 'no' || askPoker.toLowerCase() === 'n') {
+    //console.log('You really haven\'t played, have you?');
+    alert('You really haven\'t played, have you?');
+  } else {
+    //console.log('"Yes" or "No" answers only, please!');
+    alert('"Yes" or "No" answers only, please!');
+  }
+}
+
+function fifthQuestion() {
+  // Fifth Question
+  let askJavascript = prompt('Do you really think I like learning Javascript?');
+
+  if (askJavascript.toLowerCase() === 'yes' || askJavascript.toLowerCase() === 'y') {
+    //console.log('Of course I do, ' + userName + '.' + ' Who doesn\'t!?');
+    alert('Of course I do, ' + userName + '.' + ' Who doesn\'t!?');
+  } else if (askJavascript.toLowerCase() === 'no' || askJavascript.toLowerCase() === 'n') {
+    //console.log('You must not know anything about me, ' + username );
+    alert('You must not know anything about me, ' + userName + '!');
+  } else {
+    //console.log('"Yes" or "No" answers only, please!');
+    alert('"Yes" or "No" answers only!' + ' Thank you, ' + userName + '.');
+  }
+}
 /*   Sixth Question - Use FOR or WHILE LOOP
 1. Prompt user to guess a number.
 2. Indicate with alert if guess is "too high" or "too low."
@@ -134,7 +136,7 @@ let correctAttempts = 0;
 while (attemptsLeft1) {
   for (let j = 0; j < favMovies.length; j++) {
     let userResponse = prompt('Guess one of my favorite movies.');
-    if (userResponse === favMovies[0] || userResponse === favMovies[1] || userResponse === favMovies[2] ||userResponse === favMovies[3] || userResponse === favMovies[4] || userResponse === favMovies[5] || userResponse === favMovies[6] || userResponse === favMovies[7] || userResponse === favMovies[8] || userResponse === favMovies[9] ) {
+    if (userResponse === favMovies[0] || userResponse === favMovies[1] || userResponse === favMovies[2] || userResponse === favMovies[3] || userResponse === favMovies[4] || userResponse === favMovies[5] || userResponse === favMovies[6] || userResponse === favMovies[7] || userResponse === favMovies[8] || userResponse === favMovies[9]) {
       alert('You guessed one right! Try to guess another one.');
       correctAttempts++;
     } else if (userResponse !== favMovies[j]) {
